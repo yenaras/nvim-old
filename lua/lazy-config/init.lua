440e0d8eca5dd0bcee18780fa7f11dfb0ea929f7
yenaras <brandon82890@gmail.com>
2023-08-27 00:21:27 -0400

    remove old unused stuff

--- lua/lazy-config/init.lua
+++ lua/lazy-config/init.lua
@@ -57,6 +57,16 @@ local plugins = {
     "cdelledonne/vim-cmake",
     "antoinemadec/FixCursorHold.nvim",
     "NvChad/nvim-colorizer.lua",
+    'Bekaboo/dropbar.nvim',
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
 }
 
 local opts = {}
